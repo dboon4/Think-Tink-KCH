b0b7073e3c60daa04e260dad6fd693ec098e8812
dboon4 <116426219+dboon4@users.noreply.github.com>
2024-10-18 09:00:55 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,340 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-think--tink-entrance",
+      "name": "Think & Tink Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.4417025568508741,
+        "pitch": -0.0468499400967719,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4170074743102212,
+          "pitch": 0.2823335202676258,
+          "rotation": 0,
+          "target": "1-cafe-counter"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-cafe-counter",
+      "name": "Cafe Counter",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.06444292622748549,
+        "pitch": 0.003747995207742605,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1381468562604713,
+          "pitch": 0.29291780841602133,
+          "rotation": 7.0685834705770345,
+          "target": "2-cafe-seating"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-cafe-seating",
+      "name": "Cafe Seating",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.08638722342770855,
+          "pitch": 0.320409962987771,
+          "rotation": 0,
+          "target": "3-bookstore"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bookstore",
+      "name": "Bookstore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.5920693847150158,
+        "pitch": 0.016865978434841722,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5799324129871302,
+          "pitch": 0.22285258770136807,
+          "rotation": 0,
+          "target": "4-exhibition-space---entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-exhibition-space---entrance",
+      "name": "Exhibition Space - Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.004027682889217843,
+        "pitch": 0.0018739976038713024,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.045467323382109015,
+          "pitch": 0.32539552940592387,
+          "rotation": 0,
+          "target": "5-exhibition-space---centre"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.854301782003585,
+          "pitch": 0.2143056199245521,
+          "title": "Electrical Socket",
+          "text": "2 sockets per column"
+        },
+        {
+          "yaw": 0.9272138920997453,
+          "pitch": 0.17881179841589123,
+          "title": "Electrical Socket",
+          "text": "2 sockets per column"
+        },
+        {
+          "yaw": -0.016942787941506054,
+          "pitch": -0.2554321669180837,
+          "title": "Brick Wall",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "5-exhibition-space---centre",
+      "name": "Exhibition Space - Centre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.004027682889217843,
+        "pitch": -0.011249668462477658,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.006661638328179009,
+          "pitch": 0.2353719889189385,
+          "rotation": 0,
+          "target": "6-exhibition-space---end"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-exhibition-space---end",
+      "name": "Exhibition Space - End",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.07920796771824534,
+        "pitch": 0.013113615506176401,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1241864654764537,
+          "pitch": -0.15391459467862134,
+          "rotation": 0,
+          "target": "7-exhibition-space---high-level"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.40519485400445987,
+          "pitch": 0.6838018709918181,
+          "title": "Uneven Floor",
+          "text": "Sloping floor with some raised kerbs"
+        },
+        {
+          "yaw": -3.12407344998576,
+          "pitch": 0.27438697903079046,
+          "title": "Uneven Tiled Floor",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "7-exhibition-space---high-level",
+      "name": "Exhibition Space - High Level",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.27626921151227357,
+        "pitch": 0.26131855476201515,
+        "fov": 1.3474042771833745
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
